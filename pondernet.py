14b154d7130f8cded7588d688340bf18b8e0a9ab
Oscar <oscar.castro@utec.edu.pe>
2022-02-10 02:27:50 -0500

    change callback function

--- pondernet.py
+++ pondernet.py
@@ -526,13 +526,10 @@ class PonderCIFAR(pl.LightningModule):
                                        mode='max',
                                        patience=100)
 
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-
-        model_checkpoint = ModelCheckpoint(dirpath ='./model_checkpoint',
+        model_checkpoint = ModelCheckpoint(#dirpath ='./model_checkpoint',
                                            monitor ="val/accuracy",
-                                           mode    ='max',
-                                           filename='pondernet-epoch{epoch:02d}-val_loss{val/loss:.2f}')
+                                           mode    ='max'#,
+                                           #filename='pondernet-epoch{epoch:02d}-val_loss{val/loss:.2f}')
 
         return [early_stopping, model_checkpoint]
 
