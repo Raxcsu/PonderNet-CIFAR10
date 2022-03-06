6addd6ca4d6738bb7d918106d609746eece14c58
Oscar <oscar.castro@utec.edu.pe>
2022-03-06 13:00:58 -0500

    changes

--- extrapolation-resnet.py
+++ extrapolation-resnet.py
@@ -106,17 +106,9 @@ path = "CIFAR100_checkpoint/resnet-20220303-175848-epoch=60.ckpt"
 model = ResnetCIFAR.load_from_checkpoint(path)
 print(model.hparams)
 
-trainer = Trainer(
-    logger=logger,                      # W&B integration
-    gpus=-1,                            # use all available GPU's
-    max_epochs=EPOCHS,                  # maximum number of epochs
-    gradient_clip_val=GRAD_NORM_CLIP,   # gradient clipping
-    val_check_interval=0.25,            # validate 4 times per epoch
-    precision=16,                       # train in half precision
-    deterministic=True)                 # for reproducibility
-
 def main():
     for corruption in CORRUPTIONS:
+
         # initialize datamodule and model
         cifar100_dm = CIFAR100C_DataModule(
             corruption=corruption,
@@ -132,6 +124,15 @@ def main():
         # setup logger
         logger = WandbLogger(project='CIFAR100C', name=NAME, offline=False)
         logger.watch(model)
+
+        trainer = Trainer(
+            logger=logger,                      # W&B integration
+            gpus=-1,                            # use all available GPU's
+            max_epochs=EPOCHS,                  # maximum number of epochs
+            gradient_clip_val=GRAD_NORM_CLIP,   # gradient clipping
+            val_check_interval=0.25,            # validate 4 times per epoch
+            precision=16,                       # train in half precision
+            deterministic=True)                 # for reproducibility
 
         # fit the model
         # trainer.fit(model, datamodule=cifar100_dm)
